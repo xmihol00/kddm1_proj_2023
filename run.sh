8fd4ea6a75194a17e2f597ede6748c1a1280d890
Ronald Infanger <infanger@student.tugraz.at>
2023-04-23 20:58:56 +0200

    re-structure

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-
-python3 ./src/cleaning.py
-python3 ./src/train_test_split.py
-python3 ./src/missing_value_imputation.py
-python3 ./src/only_numeric_columns.py
+# pre_processing
+python3 ./src/pre_processing/01_cleaning.py
+python3 ./src/pre_processing/02_train_test_split.py
+python3 ./src/pre_processing/03_missing_value_imputation.py
+python3 ./src/pre_processing/04_normalization.py
+python3 ./src/pre_processing/05_only_numeric_columns.py
 
 # Davids Model
-python3 ./src/DNN_model.py
+# python3 ./src/DNN_model.py
