main-script for model performance average, fixed ensemble and tested on NN and RF

src/models/ensemble.py
@@ -6,15 +6,29 @@ from sklearn import metrics
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 
-# read the test predictions of all models and calculate the mean (skip the first row)
-y_pred_truth_NN = np.loadtxt("results/NN_predicted_truth.csv", delimiter=",", skiprows=1)
-y_pred_truth_RF = np.loadtxt("results/RF_predicted_truth.csv", delimiter=",", skiprows=1)
-y_pred_truth_SVR = np.loadtxt("results/SVR_predicted_truth.csv", delimiter=",", skiprows=1)
+models = [
+    "NN", 
+    "RF", 
+    #"SVR"
+]
+MSEs = []
+MAEs = []
+RMSEs = []
+R2s = []
 
-y_pred_truth = np.mean([y_pred_truth_NN, y_pred_truth_RF, y_pred_truth_SVR], axis=0)
+for i in range(5):
+    y_preds_truths = []
+    for model in models:
+        y_preds_truths.append(np.loadtxt(os.path.join("results", f"{model}_predicted_truth_seed{i}.csv"), delimiter=",", skiprows=1))
+    
+    y_pred_truth = np.mean(y_preds_truths, axis=0)
+    MSEs.append(metrics.mean_squared_error(y_pred_truth[:, 2:], y_pred_truth[:, :2]))
+    MAEs.append(metrics.mean_absolute_error(y_pred_truth[:, 2:], y_pred_truth[:, :2]))
+    RMSEs.append(np.sqrt(metrics.mean_squared_error(y_pred_truth[:, 2:], y_pred_truth[:, :2])))
+    R2s.append(metrics.r2_score(y_pred_truth[:, 2:], y_pred_truth[:, :2]))
 
 print("Ensemble model test results:")
-print(f"  - MSE: {metrics.mean_squared_error(y_pred_truth[:, 2:], y_pred_truth[:, :2])}")
-print(f"  - RMSE: {metrics.mean_squared_error(y_pred_truth[:, 2:], y_pred_truth[:, :2], squared=False)}")
-print(f"  - MAE: {metrics.mean_absolute_error(y_pred_truth[:, 2:], y_pred_truth[:, :2])}")
-print(f"  - R2 Score: {metrics.r2_score(y_pred_truth[:, 2:], y_pred_truth[:, :2])}")
+print(f"  - MSE: {np.mean(MSEs)}")
+print(f"  - MAE: {np.mean(MAEs)}")
+print(f"  - RMSE: {np.mean(RMSEs)}")
+print(f"  - R2 Score: {np.mean(R2s)}")